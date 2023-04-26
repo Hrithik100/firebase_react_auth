729c6a8e37a15360f0a43b5c6bebf0ca21994bed
Hrithik Hazarika <hrithikhazarika924@gmail.com>
2023-04-26 16:21:07 +0530

    1st commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ import { useUserAuth } from '../context/AuthContext'
 const Home = () => {
   const {user} = useUserAuth()
   return (
-    <div style={{textAlign:"center"}}>
-        <h1 style={{fontFamily:"Poppins"}}>Welcome {user?.email}</h1>
+    <div style={{textAlign:"center", marginTop:20}}>
+        <h1 style={{fontFamily:"Poppins"}}>Welcome {!user ? "guest" : `${user.email}`}</h1>
     </div>
   )
 }
--- src/pages/user/Dashboard.jsx
+++ src/pages/user/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div style={{textAlign:"center"}}>
+    <div style={{textAlign:"center", marginTop:20}}>
         <h1 style={{fontFamily:"Poppins"}}>Hello World</h1>
     </div>
   )
